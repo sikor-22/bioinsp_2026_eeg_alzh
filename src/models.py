8f3bcd5417954af90572150124b264470594f479
sikor-22 <jaqbsikora@gmail.com>
2026-01-22 18:36:38 +0100

    Simpler model for easier debug added. Parameters adjusted. Training still does not want to converge above ~75% on binary data

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from snntorch import surrogate
 from sklearn.preprocessing import StandardScaler
 
 class SNNModel(nn.Module):    
-    def __init__(self, input_size, num_hidden_layers, hidden_layers_size, output_size, num_steps, beta=0.5, threshold = 0.3):
+    def __init__(self, input_size, num_hidden_layers, hidden_layers_size, output_size, num_steps, beta=0.8, threshold = 0.2, learnable_beta = True, learnable_threshold = True):
         super(SNNModel, self).__init__()
         self.num_hidden_layers = num_hidden_layers
         self.fclayers = []
@@ -59,6 +59,50 @@ class SNNModel(nn.Module):
         return torch.stack(spk_rec, dim=0), torch.stack(mem_rec, dim=0)
     
 
+class SNNSimpleModel(nn.Module):    
+    def __init__(self, input_size, hidden_layers_size, output_size, num_steps, beta=0.8, threshold = 0.3):
+        super(SNNSimpleModel, self).__init__()       
+        self.fcfirst = nn.Linear(input_size, hidden_layers_size)
+        self.liffirst = snn.Leaky(beta=beta, spike_grad=surrogate.fast_sigmoid(), threshold = threshold)
+
+        self.fchidden = nn.Linear(hidden_layers_size, hidden_layers_size)
+        self.lifhidden = snn.Leaky(beta=beta, spike_grad=surrogate.fast_sigmoid(), threshold = threshold)
+        
+        self.fclast = nn.Linear(hidden_layers_size, output_size)
+        self.liflast = snn.Leaky(beta=beta, spike_grad=surrogate.fast_sigmoid(), threshold = threshold)
+        
+        self.num_steps = num_steps
+        self.hidden_size = hidden_layers_size
+
+        nn.init.xavier_uniform_(self.fcfirst.weight, gain=2.0)
+        nn.init.xavier_uniform_(self.fchidden.weight, gain=2.0)
+        nn.init.xavier_uniform_(self.fclast.weight, gain=2.0)
+        
+    def forward(self, x):
+        memfirst = self.liffirst.init_leaky()
+        memhidden = self.lifhidden.init_leaky()
+        memlast = self.liflast.init_leaky()
+        
+        spk_rec = []
+        mem_rec = []
+        
+        for step in range(self.num_steps):
+            cur = self.fcfirst(x[step])
+            spkfirst, memfirst = self.liffirst(cur, memfirst)
+
+            spkhidden = self.fchidden(spkfirst)
+            spkhidden, memhidden = self.lifhidden(spkhidden, memhidden)
+
+            curlast = self.fclast(spkhidden)
+            spklast, memlast = self.liflast(curlast, memlast)
+
+            spk_rec.append(spklast)
+            mem_rec.append(memlast)
+
+        
+        return torch.stack(spk_rec, dim=0), torch.stack(mem_rec, dim=0)
+    
+
 def get_model(input_size, output_size, config):
     '''
     Get SNNModel object
@@ -74,7 +118,7 @@ def get_model(input_size, output_size, config):
                     num_steps=config['num_steps'])
     return model
 
-def spike_encoding(x, method, num_steps, gain = 0.4):
+def spike_encoding(x, method, num_steps, gain = 0.45):
     '''
     Encode data to spike train
     
@@ -152,19 +196,19 @@ def debug_model_predictions(model, x_sample, y_sample, encoding_method, num_step
                 print(f"{name} - no gradient")
 
 
-def train_model(x, y, model, num_epochs, num_steps, encoding_method):
+def train_model(x, y, x_val, y_val, model, num_epochs, num_steps, encoding_method, debug_prints = False, output_temp = 5.0):
     dataset = torch.utils.data.TensorDataset(x, y)
     train_loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     loss_fn = nn.CrossEntropyLoss()
 
 
-    print("=== Pre-training Debug ===")
-    sample_idx = 0
-    debug_model_predictions(model, x[sample_idx], y[sample_idx], 
-                           encoding_method, num_steps)
+    if debug_prints:
+        sample_idx = 0
+        debug_model_predictions(model, x[sample_idx], y[sample_idx], 
+                            encoding_method, num_steps)
 
     for epoch in range(num_epochs):
         model.train()
@@ -179,8 +223,8 @@ def train_model(x, y, model, num_epochs, num_steps, encoding_method):
             spk_rec, mem_rec = model(spikes)
             
             # Train on membrane potentials
-            output = torch.sum(mem_rec, dim=0)
-            loss = loss_fn(output, batch_y)
+            output = torch.mean(mem_rec, dim=0)
+            loss = loss_fn(output/output_temp, batch_y)
             loss.backward()
             optimizer.step()
             
@@ -190,14 +234,25 @@ def train_model(x, y, model, num_epochs, num_steps, encoding_method):
         if epoch % 1 == 0:
             model.eval()
             with torch.no_grad():
+                # training set
                 x, y = x.to(device), y.to(device)
                 spikes = spike_encoding(x, encoding_method, num_steps)
                 spk_rec, mem_rec = model(spikes)
                 output = torch.sum(spk_rec, dim=0)
                 y_pred = torch.argmax(output, dim=1)
                 accuracy = torch.sum(y_pred == y).float()/len(y)
-                print(f"Epoch: {epoch} - Acc: {accuracy*100:.2f}%")
-        if epoch % 10 == 0:
+                print(f"Epoch: {epoch} - Train Acc: {accuracy*100:.2f}%")
+                # validation set
+                x_val, y_val = x_val.to(device), y_val.to(device)
+                spikes = spike_encoding(x_val, encoding_method, num_steps)
+                spk_rec, mem_rec = model(spikes)
+                output = torch.sum(spk_rec, dim=0)
+                y_pred = torch.argmax(output, dim=1)
+                val_accuracy = torch.sum(y_pred == y_val).float()/len(y_val)
+                print(f"Epoch: {epoch} - Val Acc: {val_accuracy*100:.2f}%")
+                print()
+
+        if epoch % 10 == 0 and debug_prints:
             print(f"\n=== Debug at Epoch {epoch} ===")
             debug_model_predictions(model, batch_X[0], batch_y[0], 
                                    encoding_method, num_steps)
